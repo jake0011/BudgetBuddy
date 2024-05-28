b08ee0b28fccfa75e3699df6a8b52beea69b5b81
EpaphrasSam <isinesam@gmail.com>
2024-05-28 19:25:46 +0000

    Refactored signup and login screens

--- front-end/src/app/login/index.tsx
+++ front-end/src/app/login/index.tsx
@@ -1,6 +1,13 @@
 import { Link } from "expo-router";
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 const LoginScreen: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -11,42 +18,49 @@ const LoginScreen: React.FC = () => {
   };
 
   return (
-    <View className="flex-1 justify-center gap-10 items-center bg-gray-800">
-      <View className="w-1/2 justify-center items-center">
+    <View className="flex-1 p-6 justify-center gap-10 items-center bg-gray-800">
+      <View className=" w-full justify-center items-center">
         <Image
           source={require("assets/images/expenseTracking.jpg")}
-          className="w-64 h-64 rounded-3xl"
+          className="w-full h-64 rounded-3xl"
           resizeMode="cover"
         />
         <Text className="text-2xl font-bold text-white pt-5">
           BudgetBuddy Login
         </Text>
       </View>
-      <View className="w-1/2 justify-center items-center">
-        <TextInput
-          className="w-64 h-10 border-2 border-gray-300 placeholder:text-gray-300 rounded-md mb-2 px-2"
-          placeholder="Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          className="w-64 h-10 border-2 border-gray-300  placeholder:text-gray-300 rounded-md mb-2 px-2"
-          placeholder="Password"
-          secureTextEntry
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <Link href="/signup" asChild className="justify-center my-6">
-          <TouchableOpacity
-            className="w-64 h-14 bg-slate-600 border border-gray-300 rounded-2xl px-2 mb-2"
-            onPress={handleLogin}
-          >
-            <Text className="text-white text-center rounded-xl text-xl py-3 justify-center">
-              Login
-            </Text>
-          </TouchableOpacity>
-        </Link>
+      <View className="w-full flex gap-4 justify-center items-center">
+        <View className="w-full flex-col gap-2 justify-start">
+          <Text className="text-white">Email</Text>
+          <TextInput
+            id="email"
+            className="w-full p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View className="w-full flex-col gap-2 justify-start">
+          <Text className="text-white">Password</Text>
+          <TextInput
+            id="password"
+            className="w-full p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
       </View>
+      <Link href="/signup" asChild className="justify-center">
+        <TouchableOpacity
+          className="w-full p-2 bg-slate-600 border border-gray-300 rounded-2xl px-2 mb-2"
+          onPress={handleLogin}
+        >
+          <Text className="text-white text-center rounded-xl text-xl py-3 justify-center">
+            Login
+          </Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
--- front-end/src/app/signup/index.tsx
+++ front-end/src/app/signup/index.tsx
@@ -1,79 +1,95 @@
 import { Link } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 
 const SignupScreen: React.FC = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSignup = () => {
+    // signup logic here
   };
 
-  {
-    /* Use ReactHookForm to make the form handling better */
-  }
-
   return (
-    <View className="flex-1 justify-center items-center gap-10 bg-gray-800">
-      <View className="w-1/2 justify-center gap-1 items-center bg-gray-800">
+    <View className="flex-1 p-6 justify-center gap-10 items-center bg-gray-800">
+      <View className="w-full justify-center items-center">
         <Image
           source={require("assets/images/expenseTracking.jpg")}
-          className=" w-64 h-64 rounded-3xl"
+          className="w-full h-64 rounded-3xl"
+          resizeMode="cover"
         />
-        <Text className="text-xl font-bold  py-4 color-slate-200">
+        <Text className="text-2xl font-bold text-white pt-2">
           BudgetBuddy Sign Up
         </Text>
       </View>
-      <View className="w-1/2 justify-center gap-1 items-center bg-gray-800">
-        <TextInput
-          className="w-64 h-10 border border-gray-300 placeholder:text-gray-300 rounded px-2 mb-2"
-          placeholder="First Name"
-          value={firstName}
-          onChangeText={setFirstName}
-        />
-        <TextInput
-          className="w-64 h-10 border border-gray-300 placeholder:text-gray-300 rounded px-2 mb-2"
-          placeholder="Last Name"
-          value={lastName}
-          onChangeText={setLastName}
-        />
-        <TextInput
-          className="w-64 h-10 border border-gray-300 placeholder:text-gray-300 rounded px-2 mb-2"
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          className="w-64 h-10 border border-gray-300 placeholder:text-gray-300 rounded px-2 mb-2"
-          placeholder="Password"
-          inlineImageLeft="password_icon"
-          secureTextEntry
-          value={password}
-          enablesReturnKeyAutomatically
-          onChangeText={setPassword}
-        />
-        <TextInput
-          className="w-64 h-10 border border-gray-300 placeholder:text-gray-300 rounded px-2 mb-2"
-          placeholder="Re-enter Password"
-          inlineImageLeft="password_icon"
-          secureTextEntry
-          value={password}
-          enablesReturnKeyAutomatically
-          onChangeText={setPassword}
-        />
-        <Link href="/login" asChild className="justify-center my-6">
-          <TouchableOpacity
-            className="w-64 h-14 bg-slate-600 border border-gray-300 rounded-2xl px-2 mb-2"
-            onPress={handleSignup}
-          >
-            <Text className="text-white text-center rounded-xl text-xl py-3 justify-center">
-              Sign Up
-            </Text>
-          </TouchableOpacity>
-        </Link>
+      <View className="w-full flex gap-3 justify-center items-center">
+        <View className="w-full flex-row gap-4 justify-between">
+          <View className="w-full flex-1 flex-col gap-2 justify-start">
+            <Text className="text-white">First Name</Text>
+            <TextInput
+              id="firstName"
+              className="w-full text-white p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+              placeholder="First Name"
+              value={firstName}
+              onChangeText={(text) => setFirstName(text)}
+            />
+          </View>
+          <View className="w-1/2 flex-col gap-2 justify-start">
+            <Text className="text-white">Last Name</Text>
+            <TextInput
+              id="lastName"
+              className="w-full text-white p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+              placeholder="Last Name"
+              value={lastName}
+              onChangeText={(text) => setLastName(text)}
+            />
+          </View>
+        </View>
+        <View className="w-full flex-col gap-2 justify-start">
+          <Text className="text-white">Email</Text>
+          <TextInput
+            id="email"
+            className="w-full text-white p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View className="w-full flex-col gap-2 justify-start">
+          <Text className="text-white">Password</Text>
+          <TextInput
+            id="password"
+            className="w-full text-white p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+          />
+        </View>
+        <View className="w-full flex-col gap-2 justify-start">
+          <Text className="text-white">Password</Text>
+          <TextInput
+            id="password"
+            className="w-full text-white p-3 border-2 border-gray-300 placeholder:text-gray-300 rounded-md px-2"
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+          />
+        </View>
       </View>
+      <Link href="/login" asChild className="justify-center ">
+        <TouchableOpacity
+          className="w-full p-2 bg-slate-600 border border-gray-300 rounded-2xl px-2 "
+          onPress={handleSignup}
+        >
+          <Text className="text-white text-center rounded-xl text-xl py-3 justify-center">
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
