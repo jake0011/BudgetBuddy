a43f254081e8d7dfb5b7f33f21072d23cdb2abda
kwabenadarkwa <darkwak@live.com>
2024-05-28 18:50:21 +0000

    :fire: remove index.ts file

--- back-end/index.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-//TODO: check how this thing is used to make the queries from the documentation 
-//it seems like this is  what I am going to be using to make all of the queries
-const db = drizzle(sql);
-
