af38c82ffed5efb41a9fce32799589c90f0d0d45
kwabenadarkwa <darkwak@live.com>
2024-05-30 16:50:33 +0000

    :recycle: refactor some small things I don't recall

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import { logger } from "hono/logger";
 import user from "./handlers/users";
@@ -6,6 +7,7 @@ import user from "./handlers/users";
 const app = new Hono();
 app.use(prettyJSON());
 app.use(logger());
+app.use(cors());
 app.get("/", (c) => {
   return c.json("Welcome to Budget Buddy");
 });
