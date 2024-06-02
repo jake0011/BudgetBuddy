35785dc859e7cf976f5361a5de8c46fd5fb68c4b
kwabenadarkwa <darkwak@live.com>
2024-06-02 17:32:02 +0000

    :safety_vest: add signup validation and update hashing algorithm

--- back-end/src/handlers/users.ts
+++ back-end/src/handlers/users.ts
@@ -1,7 +1,19 @@
 import { Hono } from "hono";
-import { db } from "../db/db";
+import { eq, or } from "drizzle-orm";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
 import { users } from "../db/schema/users.ts";
-import { eq, and } from "drizzle-orm";
+import { db } from "../db/db";
+
+const signUpSchema = z.object({
+  username: z.string(),
+  firstname: z.string(),
+  middlename: z.string(),
+  lastname: z.string(),
+  password: z.string().min(8),
+  email: z.string(),
+});
 
 const user = new Hono().basePath("v1/user");
 
@@ -15,40 +27,48 @@ user.get("/all", async (c) => {
 });
 
 //TODO: also do some JWT for authentication as a middleware
-//also make sure to do validation for the user input
-user.post("/signup", async (c) => {
-  try {
-    const body = await c.req.json();
-
-    //TODO: see if you can find a way to use the error rather to get it
-    const userExists = await db
-      .select()
-      .from(users)
-      .where(
-        and(eq(users.lastname, body.lastname), eq(users.email, body.email)),
-      );
-
-    if (userExists) {
-      return c.json("User Already Exists", 409);
+user.post(
+  "/signup",
+  zValidator("json", signUpSchema, (result, c) => {
+    if (!result.success) {
+      return c.json("Invalid Input", 415);
     }
+  }),
+  async (c) => {
+    try {
+      const body = await c.req.json();
 
-    const argonHash = await Bun.password.hash(body.password, {
-      memoryCost: 4,
-      timeCost: 3,
-    });
+      //TODO: see if you can find a way to use the error rather to get it
+      const userExists = await db
+        .select()
+        .from(users)
+        .where(
+          or(eq(users.username, body.username), eq(users.email, body.email)),
+        );
 
-    await db.insert(users).values({
-      username: body.username,
-      firstname: body.firstname,
-      middlename: body.middlename,
-      lastname: body.lastname,
-      password: argonHash,
-      email: body.email,
-    });
-    return c.json("Sign Up successful", 201);
-  } catch (err) {
-    return err;
-  }
-});
+      if (userExists.length != 0) {
+        return c.json("User Already Exists", 409);
+      }
+
+      //INFO: for some reason, when you use Argon2id it doesn't work
+      const bcryptHash = await Bun.password.hash(body.password, {
+        algorithm: "bcrypt",
+        cost: 4,
+      });
+
+      await db.insert(users).values({
+        username: body.username,
+        firstname: body.firstname,
+        middlename: body.middlename,
+        lastname: body.lastname,
+        password: bcryptHash,
+        email: body.email,
+      });
+      return c.json("Sign Up successful", 201);
+    } catch (err) {
+      return err;
+    }
+  },
+);
 
 export default user;
