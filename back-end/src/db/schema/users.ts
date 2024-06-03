e4250a55d707b85fe9f3d023ff0fda9c08563697
kwabenadarkwa <darkwak@live.com>
2024-06-03 19:53:46 +0000

    :poop:finish a working version of the signin(to be improved later)

--- back-end/src/db/schema/users.ts
+++ back-end/src/db/schema/users.ts
@@ -1,16 +1,21 @@
-import { pgTable, serial, varchar, timestamp, unique } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+  unique,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   userId: serial("userId").primaryKey(),
-  username: varchar("username", { length: 256 }).unique(),
-  firstname: varchar("firstname", { length: 256 }),
+  username: varchar("username", { length: 256 }).notNull().unique(),
+  firstname: varchar("firstname", { length: 256 }).notNull(),
   middlename: varchar("middlename", { length: 256 }),
-  lastname: varchar("lastname", { length: 256 }),
-  password: varchar("password", { length: 256 }),
-  email: varchar("email", { length: 256 }).unique(),
+  lastname: varchar("lastname", { length: 256 }).notNull(),
+  password: varchar("password", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull().unique(),
   // INFO: on update now is does not work with postgress
   // TODO: look for a way to make it work triggers
   // updatedAt: timestamp("updatedAt").notNull().onUpdateNow(),
   createdAt: timestamp("createdAt").notNull().defaultNow(),
 });
-
