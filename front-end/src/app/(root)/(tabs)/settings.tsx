b558f04f80fdcd022d3ea70abb11c8033912bb04
jakee <unreshablekhon@gmail.com>
2024-06-06 19:41:53 +0000

    Co-authored-by: EpaphrasSam <EpaphrasSam@users.noreply.github.com> configuring expo router for the bottom tab navigation

---
+++ front-end/src/app/(root)/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Tab() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Tab [Home|Accounts|Cash Flow|Settings]</Text>
+    </View>
+  );
+}
